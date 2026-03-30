26ad0a017531c7f8094682bae70f3a7b9ba04672
boonhapus <me@boonhap.us>
2026-03-29 20:45:57 -0500

    consolidate types

--- src/professor_pendo/types.py
+++ src/professor_pendo/types.py
@@ -1,4 +1,10 @@
-from typing import Literal
+from typing import Annotated, Any, Literal
+
+# ── General Types ─────────────────────────────────────────────────────
+
+type ExitCodeT = Annotated[int, "OS-level exit code"]
+
+# ── Pendo Types ───────────────────────────────────────────────────────
 
 type DataEnvironmentT = Literal["io", "eu", "us1", "jpn", "au"]
 """
@@ -7,3 +13,6 @@ The Pendo signifier for the GCP data region.
 Further reading:
 https://support.pendo.io/hc/en-us/articles/22832528657179-Global-data-hosting
 """
+
+type AggregationPipelineT = list[dict[str, Any]]
+"""The shape of one or many pipelines."""
